44b2cfa7fc9b065603e56381e767019ebb4290cb
Francyelli Silva <127361802+FrancyelliSilva@users.noreply.github.com>
2026-06-21 16:03:11 +0000

    Adicionando Cafés Quentes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-fetch('https://api.sampleapis.com/coffee/hot')
+// Lista de café quentes no dropdown
+fetch('https://api.sampleapis.com/coffee/hot') //api
   .then((resp) => resp.json())
   .then((data) => {
     const container = document.getElementById('lista-cafes');
     data.forEach((cafe) => {
       container.innerHTML += `
         <li class="card-cafe">
-          <img src="${cafe.image}" alt="${cafe.title}">
+          <img src="${cafe.image}" alt="${cafe.title}" onerror="this.parentElement.style.display='none'">
           <h3>${cafe.title}</h3>
-          <p>${cafe.description}</p>
         </li>
       `;
     });
@@ -15,9 +15,9 @@ fetch('https://api.sampleapis.com/coffee/hot')
   .catch((err) => console.log(err));
 
 const linkCafes = document.getElementById('link-cafes');
-const lista = document.getElementById('lista-cafes');
+const lista = document.getElementById('caixa-dropdown');
 
-lista.style.display = 'none';
+lista.style.display = 'none'; //mantém a lista escondida até clicar no link
 
 linkCafes.addEventListener('click', () => {
    if (lista.style.display === 'none') {
@@ -26,3 +26,38 @@ linkCafes.addEventListener('click', () => {
       lista.style.display = 'none';
    }
 });
+
+//Lista de Cafés Quentes para a busca
+let CafesQuentes = []
+
+function mostrarCafes(cafes){
+  const container = document.getElementById('lista-cafes');
+  container.innerHTML = ''
+  cafes.forEach((cafe) => {
+    container.innerHTML += `
+      <li class="card-cafe">
+        <img src="${cafe.image}" alt="${cafe.title}" onerror="this.parentElement.style.display='none'">
+        <h3>${cafe.title}</h3>
+      </li>
+    `;
+  });
+}
+
+fetch('https://api.sampleapis.com/coffee/hot')
+  .then((resp) => resp.json())
+  .then((data) => {
+    CafesQuentes = data;
+    mostrarCafes(CafesQuentes);
+  })
+  .catch((err) => console.log(err))
+
+  //Busca de Cafés Quentes
+  const campoBusca = document.getElementById('campo-busca');
+
+  campoBusca.addEventListener('input', () => {
+    const texto = campoBusca.value.toLowerCase();
+    const filtrados = CafesQuentes.filter((cafe) => {
+      return cafe.title.toLowerCase().includes(texto);
+    });
+    mostrarCafes(filtrados)
+  })
